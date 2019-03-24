4e10742dc09909f3a4cb8ec63fdb6d456447ab49
chris <christopherchateau@gmail.com>
2019-03-24 16:19:14 -0600

    Create Header and Standings components, import to App. Add css grid to App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Header from "../Header/";
+import Standings from "../Standings/";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <Standings />
       </div>
     );
   }
---
+++ src/components/Standings/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Standings.css';
+
+class Standings extends Component {
+  render() {
+    return (
+      <div className="Standings">
+      </div>
+    );
+  }
+}
+
+export default Standings;
