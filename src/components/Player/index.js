7394330d0665851cdaf7b7d9da12995f18181f57
chris <christopherchateau@gmail.com>
2019-03-24 17:44:45 -0600

    Add Player component

---
+++ src/components/Player/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Player.css';
+
+class Player extends Component {
+  render() {
+    return (
+      <div className="Player">
+      </div>
+    );
+  }
+}
+
+export default Player;
