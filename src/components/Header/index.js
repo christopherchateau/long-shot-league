10ad0dec38e7800fa89d4c66d1104459aa0ef126
chris <christopherchateau@gmail.com>
2019-03-24 17:10:14 -0600

    Add todd.jpg to images dir, import to Header, add position styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
-import './Header.css';
+import React, { Component } from "react";
+import toddImg from "../../images/todd.jpg";
+import "./Header.css";
 
 class Header extends Component {
   render() {
     return (
-      <div className="Header">LONG SHOT LEAGUE
+      <div className="Header">
+        <h1>
+          LONG SH
+          <img className="todd-img" src={toddImg} />
+          T LEAGUE
+        </h1>
       </div>
     );
   }
