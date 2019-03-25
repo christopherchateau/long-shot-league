dfd01b751fe3abe98c318033b31ba1ff69c09d9c
chris <christopherchateau@gmail.com>
2019-03-24 18:21:12 -0600

    Add current-standings-header section to Standings

--- src/components/Standings/index.js
+++ src/components/Standings/index.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react';
-import Player from '../Player';
-import './Standings.css';
+import React, { Component } from "react";
+import Player from "../Player";
+import "./Standings.css";
 
 class Standings extends Component {
   render() {
     return (
       <div className="Standings">
         <div className="current-standings">
+          <div className="current-standings-header">
+            <h5>rank</h5>
+            <h5>name</h5>
+            <h5>points</h5>
+          </div>
+          <Player />
+          <Player />
+          <Player />
+          <Player />
+          <Player />
+          <Player />
+          <Player />
+          <Player />
+          <Player />
+          <Player />
+          <Player />
+          <Player />
+          <Player />
+          <Player />
           <Player />
         </div>
       </div>
