7626340bbe06da9348aa7db89344897af4c6b21e
chris <christopherchateau@gmail.com>
2019-03-25 14:39:30 -0600

    Add loadPlayerData and generatePointTotal func's to MainPage, invoke in componentDidMount in order to load all player/team data initially

--- src/data/players.js
+++ src/data/players.js
@@ -1,82 +1,82 @@
 export const playerList = [
   {
     name: "Chris",
-    points: 2,
+    bonus: 2,
     teams: ["Duke", "Syracuse", "New Mexico St", "Iona"]
   },
   {
     name: "Brewers",
-    points: 3,
+    bonus: 3,
     teams: ["Kentucky", "Seton Hall", "Mississippi St", "North Dakota St"]
   },
   {
     name: "Justin",
-    points: 4,
+    bonus: 4,
     teams: ["Murray St", "Yale", "Arizona St", "Fairleigh Dickinson"]
   },
   {
     name: "Brent",
-    points: 8,
+    bonus: 8,
     teams: ["Gonzaga", "Buffalo", "Maryland", "Gardner-Webb"]
   },
   {
     name: "Alex",
-    points: 6,
+    bonus: 6,
     teams: ["Houston", "Saint Louis", "Iowa", "Abilene-Christian"]
   },
   {
     name: "Matt",
-    points: 1,
+    bonus: 1,
     teams: ["Virginia", "Nevada", "Iowa St", "Bradley"]
   },
   {
     name: "Reed",
-    points: 5,
+    bonus: 5,
     teams: ["North Carolina", "Louisville", "St Mary's", "Colgate"]
   },
   {
     name: "Todd",
-    points: 9,
+    bonus: 9,
     teams: ["Wofford", "Belmont", "UC Irvine", "Montana"]
   },
   {
     name: "Laura",
-    points: 5,
+    bonus: 5,
     teams: ["Michigan St", "Kansas St", "Washington", "VCU"]
   },
   {
     name: "Scott",
-    points: 9,
+    bonus: 9,
     teams: ["Tennessee", "Marquette", "Ohio St", "Central Florida"]
   },
   {
     name: "Doherty",
-    points: 7,
+    bonus: 7,
     teams: ["Kansas", "Virginia Tech", "Florida", "Georgia St"]
   },
   {
     name: "Kevin",
-    points: 7,
+    bonus: 7,
     teams: ["Purdue", "Wisconsin", "Northern Kentucky", "Baylor"]
   },
   {
     name: "Kristin",
-    points: 12,
+    bonus: 12,
     teams: ["Michigan", "Auburn", "Minnesota", "Mississippi"]
   },
   {
     name: "Bmase",
-    points: 12,
+    bonus: 12,
     teams: ["Florida St", "Texas Tech", "Liberty", "Utah St"]
   },
   {
     name: "Fraske",
-    points: 3,
+    bonus: 3,
     teams: ["Villanova", "Cincinnati", "Vermont", "Old Dominion"]
   },
   {
     name: "Dan",
-    points: 13,
+    bonus: 13,
     teams: ["LSU", "Oregon", "Oklahoma", "Northeastern"]
   }
 ];
