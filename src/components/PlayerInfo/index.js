d80056abe05a45345df3181637d092042abf5db2
chris <christopherchateau@gmail.com>
2019-03-25 14:52:20 -0600

    Pass selectedPlayerData as prop to MainPage. Remove redundant functionality from MainPage

--- src/components/PlayerInfo/index.js
+++ src/components/PlayerInfo/index.js
@@ -1,27 +1,16 @@
 import React, { Component } from "react";
 import playerImg from "../../images/player.svg";
-import { playerList } from "../../data/players.js";
-import { teamList } from "../../data/teams.js";
 import "./PlayerInfo.css";
 
 class PlayerInfo extends Component {
   render() {
-    const { selectedPlayer, handleBackClick } = this.props;
-
-    const selectedTeamsNames = playerList.find(
-      player => player.name === selectedPlayer
-    ).teams;
-
-    const selectedTeamsData = teamList.filter(listTeam =>
-      selectedTeamsNames.includes(listTeam.name)
-    );
-
+    const { selectedPlayerData, handleBackClick } = this.props;
     return (
       <div className="PlayerInfo">
-        <h1 className="player-info-name">{selectedPlayer}</h1>
+        <h1 className="player-info-name">{selectedPlayerData.name}</h1>
         <img className="player-info-pic" src={playerImg} />
         <div className="player-info-teams">
-          {selectedTeamsData.map(team => (
+          {selectedPlayerData.teams.map(team => (
             <h5
               className={"team".concat(team.eliminated ? " eliminated" : "")}
               key={team.name}
