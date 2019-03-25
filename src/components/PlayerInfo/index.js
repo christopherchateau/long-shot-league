e06e0067bd691e535139d47233a6d722705d096b
chris <christopherchateau@gmail.com>
2019-03-24 23:49:06 -0600

    Add PlayerInfo component

---
+++ src/components/PlayerInfo/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./PlayerInfo.css";
+
+class PlayerInfo extends Component {
+  render() {
+    return (
+      <div className="PlayerInfo">PlayerInfo
+      </div>
+    );
+  }
+}
+
+export default PlayerInfo;
