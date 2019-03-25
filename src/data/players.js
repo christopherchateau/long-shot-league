2b5a1346224ac5caa99666bcdae8731728a50f42
chris <christopherchateau@gmail.com>
2019-03-24 18:57:53 -0600

    Add players.js to data dir with names and teams

---
+++ src/data/players.js
@@ -0,0 +1,66 @@
+export const players = [
+  {
+    name: "Chris",
+    teams: ["Duke", "Syracuse", "New Mexico State", "Iona"]
+  },
+  {
+    name: "Brewers",
+    teams: ["Kentucky", "Seton Hall", "Mississippo State", "North Dakota State"]
+  },
+  {
+    name: "Justin",
+    teams: ["Murray State", "Yale", "Arizona State", "Fairleigh Dickinson"]
+  },
+  {
+    name: "Brent",
+    teams: ["Gonzaga", "Buffalo", "Maryland", "Gardner-Webb"]
+  },
+  {
+    name: "Alex",
+    teams: ["Houston", "Saint Louis", "Iowa", "Abilene-Christian"]
+  },
+  {
+    name: "Matt",
+    teams: ["Virginia", "Nevada", "Iowa State", "Bradley"]
+  },
+  {
+    name: "Reed",
+    teams: ["North Carolina", "Louisville", "St. Mary's", "Colgate"]
+  },
+  {
+    name: "Todd",
+    teams: ["Wofford", "Belmont", "UC Irvine", "Montana"]
+  },
+  {
+    name: "Laura",
+    teams: ["Michigan State", "Kansas State", "Washington", "VCU"]
+  },
+  {
+    name: "Scott",
+    teams: ["Tennessee", "Marquette", "Ohio State", "Central Florida"]
+  },
+  {
+    name: "Doherty",
+    teams: ["Kansas", "Virgina Tech", "Florida", "Georgia State"]
+  },
+  {
+    name: "Kevin",
+    teams: ["Purdue", "Wisconsin", "Northern Kentucky", "Baylor"]
+  },
+  {
+    name: "Kristin",
+    teams: ["Michigan", "Auburn", "Minnesota", "Mississippi"]
+  },
+  {
+    name: "Bmase",
+    teams: ["Florida State", "Texas Tech", "Liberty", "Utah State"]
+  },
+  {
+    name: "Fraske",
+    teams: ["Villanova", "Cincinnati", "Vermont", "Old Dominion"]
+  },
+  {
+    name: "Dan",
+    teams: ["LSU", "Oregon", "Oklahoma", "Northeastern"]
+  }
+];
