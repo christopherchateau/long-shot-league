ee3f325e43f0af13ee72b654605361c6aeda4d62
chris <christopherchateau@gmail.com>
2019-03-25 11:39:28 -0600

    Add MainPage component to hold PlayerInfo and Standings

--- src/components/Standings/index.js
+++ src/components/Standings/index.js
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
 import Player from "../Player";
+// import PlayerInfo from "../PlayerInfo";
 import { playerList } from "../../data/players.js";
 import "./Standings.css";
 
 class Standings extends Component {
   render() {
     const playerListSorted = playerList.sort((a, b) => {
-      if (a.points > b.points) {
-        return -1;
-      }
-      if (a.points < b.points) {
-        return 1;
-      }
+      if (a.points > b.points) return -1;
+      if (a.points < b.points) return 1;
     });
 
     const players = playerListSorted.map((player, i) => (
@@ -20,14 +17,12 @@ class Standings extends Component {
 
     return (
       <div className="Standings">
-        <div className="current-standings">
-          <div className="current-standings-header">
-            <h5>rank</h5>
-            <h5>name</h5>
-            <h5 className="current-standings-points">points</h5>
-          </div>
-          {players}
+        <div className="current-standings-header">
+          <h5>rank</h5>
+          <h5>name</h5>
+          <h5 className="current-standings-points">points</h5>
         </div>
+        {players}
       </div>
     );
   }
