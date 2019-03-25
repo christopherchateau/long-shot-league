4c337d8fdb9502caf1600028e19d7893a3c5b0a4
chris <christopherchateau@gmail.com>
2019-03-24 21:08:09 -0600

    Updates

---
+++ precache-manifest.9904b54358f6f899d21e03e32d241926.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "401918f05dbe47eb25e377818b092116",
+    "url": "/long-shot-league/static/media/todd.401918f0.png"
+  },
+  {
+    "revision": "f29f6e97a1c2b958dc6d01d490aa1327",
+    "url": "/long-shot-league/static/media/player.f29f6e97.svg"
+  },
+  {
+    "revision": "c0cd643479418d78b4069675d20c5093",
+    "url": "/long-shot-league/static/media/headshot.c0cd6434.jpg"
+  },
+  {
+    "revision": "58e6fc112b41c0948b77a1fdad30e50c",
+    "url": "/long-shot-league/static/media/court.58e6fc11.jpg"
+  },
+  {
+    "revision": "e7331fb8f4a17e3eaa2f",
+    "url": "/long-shot-league/static/js/runtime~main.9e0388a0.js"
+  },
+  {
+    "revision": "90feb229fa87fdf5bc5d",
+    "url": "/long-shot-league/static/js/main.df460a41.chunk.js"
+  },
+  {
+    "revision": "17d97636303d8aae4b91",
+    "url": "/long-shot-league/static/js/2.1128e7e4.chunk.js"
+  },
+  {
+    "revision": "90feb229fa87fdf5bc5d",
+    "url": "/long-shot-league/static/css/main.97efc49d.chunk.css"
+  },
+  {
+    "revision": "1611ef04dcf69134114f6a5fa5860f37",
+    "url": "/long-shot-league/index.html"
+  }
+];
