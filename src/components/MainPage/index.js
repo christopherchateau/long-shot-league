76cd39647ae9640102bd4d40aa751f43ea89f1ac
chris <christopherchateau@gmail.com>
2019-03-26 10:19:07 -0600

    Move api call functions from MainPage to new apiCalls dir

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -16,22 +16,6 @@ class MainPage extends Component {
     await this.loadPlayerData(playerList, teamList);
   };
 
-  getPlayers = async () => {
-    const response = await fetch(
-      "https://long-shot-league-be.herokuapp.com/api/v1/longshotleague/players"
-    );
-    const data = await response.json();
-    return data;
-  };
-
-  getTeams = async () => {
-    const response = await fetch(
-      "https://long-shot-league-be.herokuapp.com/api/v1/longshotleague/teams"
-    );
-    const data = await response.json();
-    return data;
-  };
-
   loadPlayerData = (playerList, teamList) => {
     const players = [];
 
