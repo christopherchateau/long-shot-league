01ae56082630a204b8a7e513e79a3d72f5056aa8
chris <christopherchateau@gmail.com>
2019-03-25 18:01:20 -0600

    Import pics into PlayersInfo

--- src/components/PlayerInfo/index.js
+++ src/components/PlayerInfo/index.js
@@ -1,14 +1,47 @@
 import React, { Component } from "react";
-import playerImg from "../../images/player.svg";
+import Dan from "../../images/player.svg";
+import Bmase from "../../images/player.svg";
+import Scott from "../../images/player.svg";
+import Kristin from "../../images/player.svg";
+import Alex from "../../images/player.svg";
+import Doherty from "../../images/player.svg";
+import Kevin from "../../images/player.svg";
+import Todd from "../../images/todd.png";
+import Laura from "../../images/player.svg";
+import Brent from "../../images/player.svg";
+import Reed from "../../images/player.svg";
+import Justin from "../../images/player.svg";
+import Brewers from "../../images/player.svg";
+import Matt from "../../images/player.svg";
+import Fraske from "../../images/player.svg";
+import Chris from "../../images/headshot.jpg";
 import "./PlayerInfo.css";
 
 class PlayerInfo extends Component {
   render() {
     const { selectedPlayerData, handleBackClick } = this.props;
+    const playerPics = {
+      Dan,
+      Bmase,
+      Scott,
+      Kristin,
+      Alex,
+      Doherty,
+      Kevin,
+      Todd,
+      Laura,
+      Brent,
+      Reed,
+      Justin,
+      Brewers,
+      Matt,
+      Fraske,
+      Chris
+    };
     return (
       <div className="PlayerInfo">
         <h1 className="player-info-name">{selectedPlayerData.name}</h1>
-        <img className="player-info-pic" src={playerImg} />
+        <img className="player-info-pic" src={playerPics[selectedPlayerData.name]} />
         <div className="player-info-totals">
           <h5>
             points
