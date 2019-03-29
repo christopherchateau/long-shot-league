f2f97d7b62680459acbcd9f6b1f00c17e920dcc7
chris <christopherchateau@gmail.com>
2019-03-29 11:24:12 -0600

    Add handleNavBarClick to App

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,7 @@ import "./Header.css";
 
 class Header extends Component {
   render() {
+    const { handleNavBarClick } = this.props;
     return (
       <div className="Header">
         <div className="header-logo">
@@ -12,13 +13,26 @@ class Header extends Component {
             <img className="todd-img" src={toddImg} />T LEAGUE
           </h1>
         </div>
-        
+
         <div className="header-nav">
-
-        <h3 className="header-nav-link">standings</h3>
-        <h3 className="header-nav-link">teams</h3>
-        <h3 className="header-nav-link">bonus</h3>
-
+          <h3
+            className="header-nav-link"
+            onClick={() => handleNavBarClick("standings")}
+          >
+            standings
+          </h3>
+          <h3
+            className="header-nav-link"
+            onClick={() => handleNavBarClick("teams")}
+          >
+            teams
+          </h3>
+          <h3
+            className="header-nav-link"
+            onClick={() => handleNavBarClick("bonus")}
+          >
+            bonus
+          </h3>
         </div>
       </div>
     );
