cb587001f42293fd55c0e84f89dd137c6bdd7562
chris <christopherchateau@gmail.com>
2019-03-29 11:11:26 -0600

    Add nav section to Header with styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,11 +6,20 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <h1>
-          LONG SH
-          <img className="todd-img" src={toddImg} />
-          T LEAGUE
-        </h1>
+        <div className="header-logo">
+          <h1>
+            LONG SH
+            <img className="todd-img" src={toddImg} />T LEAGUE
+          </h1>
+        </div>
+        
+        <div className="header-nav">
+
+        <h3 className="header-nav-link">standings</h3>
+        <h3 className="header-nav-link">teams</h3>
+        <h3 className="header-nav-link">bonus</h3>
+
+        </div>
       </div>
     );
   }
