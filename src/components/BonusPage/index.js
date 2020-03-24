9eb84380df40667a17e80de40d7ca2001d7fc0e4
chris <christopherchateau@gmail.com>
2020-03-23 19:21:38 -0600

    refactor BonusPage

--- src/components/BonusPage/index.js
+++ src/components/BonusPage/index.js
@@ -1,47 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Bonus from '../Bonus'
 import loadingImg from '../../assets/images/loading.gif'
 import './BonusPage.css'
 
-class BonusPage extends Component {
-    sortByName = input =>
+export default ({ players, bonusList }) => {
+    const sortByName = input =>
         input.sort((a, b) => {
             if (a.name < b.name) return -1
             if (a.name > b.name) return 1
         })
 
-    render() {
-        let { players, bonusList } = this.props
+    const playersDisplayed = sortByName(players).map(player => {
+        const bonusListDisplayed = []
 
-        const playersDisplayed = this.sortByName(players).map(player => {
-            const bonusListDisplayed = []
+        const playerBonusTotal = bonusList.reduce((total, bonus) => {
+            if (bonus.name === player.name) {
+                bonusListDisplayed.push(
+                    <Bonus bonusList={bonus} key={bonus.id} />
+                )
+                total += bonus.points
+            }
+            return +total
+        }, 0)
 
-            const playerBonusTotal = bonusList.reduce((total, bonus) => {
-                if (bonus.name === player.name) {
-                    bonusListDisplayed.push(
-                        <Bonus bonusList={bonus} key={bonus.id} />
-                    )
-                    total += bonus.points
-                }
-                return +total
-            }, 0)
+        return <div className='bonus-player' key={player.name}>
 
-            return (
-                <div className='bonus-player' key={player.name}>
-                    <h3 className='bonus-player-name'>{`${player.name} - ${playerBonusTotal}`}</h3>
-                    <div>{bonusListDisplayed}</div>
-                </div>
-            )
-        })
+            <h3 className='bonus-player-name'>{`${player.name} - ${playerBonusTotal}`}</h3>
+            <div>{bonusListDisplayed}</div>
 
-        if (!players.length) {
-            return (
-                <img className='loading-img' src={loadingImg} alt='loading' />
-            )
-        } else {
-            return <div className='BonusPage'>{playersDisplayed}</div>
-        }
-    }
+        </div>
+    })
+
+    return !players.length
+        ? <img className='loading-img' src={loadingImg} alt='loading' />
+        : <div className='BonusPage'>{playersDisplayed}</div>
 }
-
-export default BonusPage
