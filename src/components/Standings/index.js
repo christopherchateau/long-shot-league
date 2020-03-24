6f399ec2b4dbdf2891fc89514140e3a9b62c76d0
chris <christopherchateau@gmail.com>
2020-03-23 19:11:43 -0600

    refactor Standings

--- src/components/Standings/index.js
+++ src/components/Standings/index.js
@@ -1,50 +1,40 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Player from '../Player'
 import loadingImg from '../../assets/images/loading.gif'
+
 import './Standings.css'
 
-class Standings extends Component {
-    render() {
-        const { players, handlePlayerClick } = this.props
+export default ({ players, handlePlayerClick }) => {
+    const playerListSorted = players.sort((a, b) => {
+        if (a.pointTotal > b.pointTotal) return -1
+        if (a.pointTotal < b.pointTotal) return 1
+    })
 
-        const playerListSorted = players.sort((a, b) => {
-            if (a.pointTotal > b.pointTotal) return -1
-            if (a.pointTotal < b.pointTotal) return 1
-        })
+    let counter = 1
+    const formattedPlayers = playerListSorted.map((player, i) => {
+        i > 0 && player.pointTotal === playerListSorted[i - 1].pointTotal
+            ? counter++
+            : counter = 0
 
-        let counter = 1
-        const formattedPlayers = playerListSorted.map((player, i) => {
-            i > 0 && player.pointTotal === playerListSorted[i - 1].pointTotal
-                ? counter++
-                : (counter = 0)
+        return <Player
+            handlePlayerClick={handlePlayerClick}
+            {...player}
+            rank={i + 1 - counter}
+            key={player.name}
+        />
+    })
 
-            return (
-                <Player
-                    handlePlayerClick={handlePlayerClick}
-                    {...player}
-                    rank={i + 1 - counter}
-                    key={player.name}
-                />
-            )
-        })
-
-        if (!players.length) {
-            return (
-                <img className='loading-img' alt='loading' src={loadingImg} />
-            )
-        } else {
-            return (
-                <div className='Standings'>
-                    <div className='current-standings-header'>
-                        <h5>rank</h5>
-                        <h5>name</h5>
-                        <h5 className='current-standings-points'>points</h5>
-                    </div>
-                    {formattedPlayers}
+    return !players.length
+        ? <img className='loading-img' src={loadingImg} alt='loading' />
+        : <div className='Standings'>
+    
+                <div className='current-standings-header'>
+                    <h5>rank</h5>
+                    <h5>name</h5>
+                    <h5 className='current-standings-points'>points</h5>
                 </div>
-            )
-        }
-    }
-}
 
-export default Standings
+                {formattedPlayers}
+
+            </div>
+}
