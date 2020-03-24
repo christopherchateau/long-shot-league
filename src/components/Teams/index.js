a3891df3cad8c75f473d6928319831ed79001ed7
chris <christopherchateau@gmail.com>
2020-03-23 19:17:33 -0600

    refactor Teams

--- src/components/Teams/index.js
+++ src/components/Teams/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import loadingImg from '../../assets/images/loading.gif'
+
 import './Teams.css'
 
-class Teams extends Component {
+export default class Teams extends Component {
     state = {
         display: 'show all',
         teamSort: 'alphabetical',
@@ -30,9 +31,10 @@ class Teams extends Component {
             if (a.drafted_by > b.drafted_by) return 1
         })
 
-    render() {
+    render = () => {
         let { teamList, sortByName } = this.props
         const { display, teamSort } = this.state
+
         if (display === 'still alive') {
             teamList = teamList.filter(team => !team.is_eliminated)
         }
@@ -41,7 +43,7 @@ class Teams extends Component {
             ? this.sortByPlayer(teamList)
             : sortByName(teamList)
 
-        const teams = teamList.map(team => (
+        const teams = teamList.map(team =>
             <div
                 className={'team'.concat(
                     team.is_eliminated ? ' red' : ' green'
@@ -53,35 +55,31 @@ class Teams extends Component {
                 </h3>
                 <h5>{team.drafted_by}</h5>
             </div>
-        ))
+        )
 
-        if (!teamList.length) {
-            return (
-                <img className='loading-img' src={loadingImg} alt='loading' />
-            )
-        } else {
-            return (
-                <div className='Teams'>
-                    <div className='team-btn-wrapper'>
-                        <button
-                            className='teams-toggle-btn'
-                            onClick={this.toggleTeamDisplay}
-                        >
-                            {display}
-                        </button>
-                        <button
-                            className='teams-display-btn'
-                            onClick={this.toggleTeamSort}
-                        >
-                            {teamSort}
-                        </button>
-                    </div>
-                    <br />
-                    <div className='teams-wrapper'>{teams}</div>;
+        return !teamList.length
+            ? <img className='loading-img' src={loadingImg} alt='loading' />
+            : <div className='Teams'>
+
+                <div className='team-btn-wrapper'>
+                    <button
+                        className='teams-toggle-btn'
+                        onClick={this.toggleTeamDisplay}
+                    >
+                        {display}
+                    </button>
+                    <button
+                        className='teams-display-btn'
+                        onClick={this.toggleTeamSort}
+                    >
+                        {teamSort}
+                    </button>
                 </div>
-            )
-        }
+
+                <br />
+
+                <div className='teams-wrapper'>{teams}</div>
+
+            </div>
     }
 }
-
-export default Teams
