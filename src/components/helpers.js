e8ff5c740f910cc61465395488665ad6e33f397f
chris <christopherchateau@gmail.com>
2020-03-27 16:54:29 -0600

    refactor sortByKey fn

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,9 +1,10 @@
 /* eslint-disable */
 
 export const sortByKey = (input, key = 'name', k = formatKey(key)) =>
-    input.sort((a, b) => {
-        if (a[k] < b[k]) return -1
-        if (a[k] > b[k]) return 1
-    })
+    input.sort((a, b) =>
+        (a[k] < b[k])
+            ? -1
+            : 1
+    )
 
 const formatKey = input => input.replace(/\s/g, '_')
