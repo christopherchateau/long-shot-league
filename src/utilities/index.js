b10286379dc1a6734ea0a71238c87784646c4048
chris <christopherchateau@gmail.com>
2020-03-30 18:54:11 -0600

    fix broken apiCalls path

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,11 +1,11 @@
 let endpoint 
-// endpoint = 'http://localhost:3001/api/v1/longshotleague/'
-endpoint = 'https://long-shot-league-be.herokuapp.com/api/v1/longshotleague/'
+// endpoint = 'http://localhost:3001/api/v1/longshotleague'
+endpoint = 'https://long-shot-league-be.herokuapp.com/api/v1/longshotleague'
 
 const paths = ['players', 'teams', 'bonus']
 
 const get = async path => {
-    const response = await fetch(`${endpoint}${path}`)
+    const response = await fetch(`${endpoint}/${path}`)
     return await response.json()
 }
 
