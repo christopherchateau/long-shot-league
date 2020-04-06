26164f29ec15651e080ed947f4fa4603a892c352
chris <christopherchateau@gmail.com>
2020-04-06 12:17:29 -0600

    refactor DisplayContext functions

--- src/context/DisplayContext.js
+++ src/context/DisplayContext.js
@@ -3,19 +3,14 @@ import React, { createContext, useState } from 'react'
 export const DisplayContext = createContext()
 
 const DisplayContextProvider = props => {
-	const [pageDisplay, setPageDisplay] = useState('teams')
-	const [standingsDisplay, setStandingsDisplay] = useState('standings')
+	const [pageDisplay, setPageDisplay] = useState('standings')
 	const [selectedPlayer, setSelectedPlayer] = useState(null)
 
-	const openPlayerProfile = player => {
-		setSelectedPlayer(player)
-		setStandingsDisplay('player info')
-	}
+	const standingsDisplay = selectedPlayer ? 'player info' : 'standings'
 
-	const closePlayerProfile = () => {
-		setSelectedPlayer(null)
-		setStandingsDisplay('standings')
-	}
+	const openPlayerProfile = player => setSelectedPlayer(player)
+
+	const closePlayerProfile = () => setSelectedPlayer(null)
 
 	return (
 		<DisplayContext.Provider
