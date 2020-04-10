005b8bdf2a705359b367eaaebef73778fe8bf25c
chris <christopherchateau@gmail.com>
2020-04-09 19:22:25 -0600

    Updates

---
+++ precache-manifest.157b91a3ab1dde29af35b5f4e741af1e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa20e6781ca5837d687a3bffbc885cfc",
+    "url": "/long-shot-league/static/media/matt.fa20e678.jpg"
+  },
+  {
+    "revision": "39f8ebc43a5c82fdb827",
+    "url": "/long-shot-league/static/css/main.8fa8af90.chunk.css"
+  },
+  {
+    "revision": "e7331fb8f4a17e3eaa2f",
+    "url": "/long-shot-league/static/js/runtime~main.9e0388a0.js"
+  },
+  {
+    "revision": "957c5ed74c6e75777f27",
+    "url": "/long-shot-league/static/js/2.3c7a4116.chunk.js"
+  },
+  {
+    "revision": "69f8a180bc5167bb4717525face0420a",
+    "url": "/long-shot-league/static/media/brent.69f8a180.jpg"
+  },
+  {
+    "revision": "6361814918f6d3ec1797566e51376b46",
+    "url": "/long-shot-league/static/media/Chris.63618149.jpg"
+  },
+  {
+    "revision": "39f8ebc43a5c82fdb827",
+    "url": "/long-shot-league/static/js/main.843afd43.chunk.js"
+  },
+  {
+    "revision": "49b338aa53559bef4cababbb4769c0bf",
+    "url": "/long-shot-league/static/media/scott.49b338aa.jpg"
+  },
+  {
+    "revision": "401918f05dbe47eb25e377818b092116",
+    "url": "/long-shot-league/static/media/todd.401918f0.png"
+  },
+  {
+    "revision": "f29f6e97a1c2b958dc6d01d490aa1327",
+    "url": "/long-shot-league/static/media/player.f29f6e97.svg"
+  },
+  {
+    "revision": "a21746f5374859d98f7528e1857e3532",
+    "url": "/long-shot-league/static/media/loading.a21746f5.gif"
+  },
+  {
+    "revision": "58e6fc112b41c0948b77a1fdad30e50c",
+    "url": "/long-shot-league/static/media/court.58e6fc11.jpg"
+  },
+  {
+    "revision": "1c45dfed563649d989a3238725844b1c",
+    "url": "/long-shot-league/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/long-shot-league/precache-manifest.92b391009150eb25486e6db6a6a95443.js"
+  "/long-shot-league/precache-manifest.157b91a3ab1dde29af35b5f4e741af1e.js"
 );
 
 workbox.clientsClaim();
