8509d81de9a2df94f8bb94b37a5103b3cb770811
chris <christopherchateau@gmail.com>
2021-03-20 12:09:08 -0600

    add paul davis default pic

--- src/assets/playerPics.js
+++ src/assets/playerPics.js
@@ -3,7 +3,7 @@ import Chris from '../assets/images/Chris.jpg'
 import Matt from '../assets/images/matt.jpg'
 import Scott from '../assets/images/scott.jpg'
 import Todd from '../assets/images/todd.png'
-import playerImg from '../assets/images/player.svg'
+import playerImg from '../assets/images/paul-davis.jpg'
 
 export default {
 	Brent,
