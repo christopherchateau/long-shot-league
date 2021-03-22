264e6afca36b26794ebc1d784f3efbf230f72edd
chris <christopherchateau@gmail.com>
2021-03-21 18:05:17 -0600

    redo player info for mobile

--- src/components/PlayerInfo/index.js
+++ src/components/PlayerInfo/index.js
@@ -18,25 +18,28 @@ export default () => {
 		closePlayerProfile,
 	} = useContext(DisplayContext)
 
+	const pic = playerPics[name] || playerPics.playerImg
+
 	const bonus = !bonuses.length
 		? '-none-'
-		: bonuses.map(({ description, points }) =>
+		: bonuses.map(({ description, points }) => (
 				<h5 key={description}>{`${description} - ${points}pt`}</h5>
-		  )
+		  ))
 
 	return (
 		<div className={'PlayerInfo'.concat(showModal ? ' showModal' : '')}>
+			<div
+				className='background-img'
+				style={{ backgroundImage: `url(${pic})` }}
+			/>
+
 			<button className='back-btn' onClick={closePlayerProfile}>
 				X
 			</button>
 
 			<h1 className='player-info-name'>{name}</h1>
 
-			<img
-				className='player-info-pic'
-				src={playerPics[name] || playerPics.playerImg}
-				alt='player avatar'
-			/>
+			<img className='player-info-pic' src={pic} alt='player avatar' />
 
 			<div className='player-info-totals'>
 				<h5>
@@ -58,9 +61,10 @@ export default () => {
 			<div className='player-info-teams'>
 				{teams.map(({ name, points, is_eliminated }) => (
 					<h5
-						className={is_eliminated === 'TRUE' ? 'eliminated' : null}
-						key={name}
-					>
+						className={
+							is_eliminated === 'TRUE' ? 'eliminated' : null
+						}
+						key={name}>
 						{`${name} (${points})`}
 					</h5>
 				))}
@@ -70,7 +74,6 @@ export default () => {
 				<h3 className='player-info-bonus-title'>bonus</h3>
 
 				{bonus}
-
 			</div>
 		</div>
 	)
