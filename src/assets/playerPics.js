b8e1abf64d861d1a5b543aac55b6dbb0d5b83fe2
chris <christopherchateau@gmail.com>
2023-03-19 20:56:31 -0600

    joey hauser lfg

--- src/assets/playerPics.js
+++ src/assets/playerPics.js
@@ -1,5 +1,5 @@
 import Brent from '../assets/images/brent.jpg'
-import Chris from '../assets/images/Chris.jpg'
+import Chris from '../assets/images/ChrisJH.jpeg'
 import Matt from '../assets/images/matt.png'
 import Scott from '../assets/images/scott.jpg'
 import Todd from '../assets/images/todd.png'
