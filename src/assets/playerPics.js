1300fd70ff9d7c67852995d5675cdc8d295dbfc7
Chris Chateau <Chris.Chateau@vizio.com>
2024-03-20 11:28:48 -0600

    more pics

--- src/assets/playerPics.js
+++ src/assets/playerPics.js
@@ -3,9 +3,12 @@ import Chris from '../assets/images/Chris.jpg'
 import Matt from '../assets/images/matt.png'
 import Scott from '../assets/images/scott.jpg'
 import Todd from '../assets/images/todd.png'
-import Reed from '../assets/images/reed.png'
+import Reed from '../assets/images/reed.jpg'
 import Alex from '../assets/images/alex.png'
 import Mike from '../assets/images/mike.jpeg'
+import Patel from '../assets/images/patel.jpg'
+import Brian from '../assets/images/brian.jpg'
+import Batey from '../assets/images/batey.jpg'
 import playerImg from '../assets/images/paul-davis.jpg'
 
 export default {
@@ -17,5 +20,8 @@ export default {
 	Reed,
 	Alex,
 	Mike,
+	Patel,
+	Brian,
+	Batey,
 	playerImg,
 }
